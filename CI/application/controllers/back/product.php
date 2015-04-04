b48d4f2a0b7346e180520f4d7c68e9cea86962c1
YujieYan <klaineyan@gmail.com>
2015-04-04 00:23:54 -0400

    test product

--- CI/application/controllers/back/product.php
+++ CI/application/controllers/back/product.php
@@ -5,4 +5,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+class Product extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('product_model');
+    }
+    
+    //test can get Product by Id return as CI Array 
+    public function getProductById() {
+       $result = $this->product_model->getProductById(21354);
+       var_dump($result);
+    }
+}
